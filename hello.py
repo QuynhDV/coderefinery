d41847a117d24e9f239d996912437d81b13c7ec8
QuynhDang <linhtinhthu1122@gmail.com>
2016-12-14 12:06:06 +0200

    change both hello and runner

--- hello.py
+++ hello.py
@@ -2,5 +2,10 @@
 def say_hello():
 	print("Good midday World")
 	print("Hello World")
-	
+
+def say_something(something):
+	if something:
+		print(" ".join(something))
+	else:
+		print("Hello World!")
 ##change more
